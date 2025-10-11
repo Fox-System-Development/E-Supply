474a0da4356088a5c39d666c4db1fe55a980abc6
davimp3 <davi.nepomuceno@icloud.com>
2025-10-11 18:54:56 -0300

    [FEAT](home/inde.blade) desenvolvimento frontend tela de menu princial

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,17 +1,39 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Página Inicial') }}
-        </h2>
+    <x-slot name="header">        
+        <h1 class="home-page-tittle">
+            {{ __('Menu de Gestão 
+                Simplificada.') }}
+        </h1>
     </x-slot>
+            
+    <div class="bg-orange-middle-page">
+        <div class="main flex flex-row justify-between gap-8">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    Aqui o menu
+                
+                <div class="ml-6 w-1/4 bg-white rounded-lg p-20 transaction-block shadow-lg mt-12">
+                    <img class ="add-sign-image"src="" alt="">
+                    <p class ="main-new-transaction-title">Nova Transação</p>
+                    <p class="main-new-transaction-sub-title">Registre uma nova despesa ou receita.                    
                 </div>
-            </div>
-        </div>
+
+                <div class="w-1/4 bg-white rounded-lg p-20 extract-block shadow-lg mt-12">
+                    <img src="" alt="" class="clock-img">
+                    <p class="main-extract-title">Extrato</p>
+                    <p class="main-extract-sub-title">Acesse o histórico completo de suas movimentações.
+                </div>
+
+                <div class="w-1/4 bg-white rounded-lg p-20 edit-transaction-block shadow-lg mt-12">
+                    <img src="" alt="" class="edit-img">
+                    <p class="main-edit-transaction-title">Editar Transação</p>
+                    <p class="main-edit-transaction-sub-title">Edite informações de uma despesa ou receita já criada.             
+                </div>
+
+                <div class="w-1/4 me-6 bg-white rounded-lg p-20 config-block shadow-lg mt-12">
+                    <img src="" alt="" class="config-img">
+                    <p class="main-home-congig-title bold">Configurações</p>
+                    <p class="main-home-congig-sub-title">Organize acessos, privilégios e ajustes do sistema</p>
+                </div>
+             </div>            
+         </div>            
     </div>
 </x-app-layout>
