51bdadb2f0804f4e7b9b61e6880f5d4a796d6a68
davimp3 <davi.nepomuceno@icloud.com>
2025-10-11 20:14:06 -0300

    [FEAT](home/index.blade) desenvolvimento frontend tela de menu princial

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,30 +8,32 @@
     <div class="bg-orange-middle-page py-10">
         <div class="main flex flex-row justify-center gap-8 px-4">
 
-            <div class="w-1/4 bg-white rounded-lg p-20 shadow-lg flex flex-col items-start gap-3">
+            <div class="w-1/4 bg-white rounded-3xl p-20 shadow-2xl flex flex-col items-start gap-3 hover:bg-sky-700">
                 <img src="{{asset('images/plus.png')}}" alt="" class="h-15 w-14">
                 <p class="font-bold text-xl">Nova Transação</p>
                 <p class="text-lg text-gray-600">Registre uma nova despesa ou receita.</p>
             </div>
 
-            <div class="w-1/4 bg-white rounded-lg p-20 shadow-lg flex flex-col items-start gap-3 ">
+            <div class="w-1/4 bg-white rounded-3xl p-20 shadow-2xl flex flex-col items-start gap-3 ">
                 <img src="{{ asset('images/clock-image.png') }}" alt="Ícone de Relógio" class="h-15 w-14">
                 <p class="font-bold text-2xl">Extrato</p>
                 <p class="text-lg text-gray-600">Acesse o histórico completo de suas movimentações.</p>
             </div>
 
-            <div class="w-1/4 bg-white rounded-lg p-20 shadow-lg flex flex-col items-start gap-3">
+            <div class="w-1/4 bg-white rounded-3xl p-20 shadow-2xl flex flex-col items-start gap-3">
                 <img src="{{asset('images/edit-image.png')}}" alt="" class="h-15 w-14">
                 <p class="font-bold text-2xl">Editar Transação</p>
                 <p class="text-lg text-gray-600">Edite informações de uma despesa ou receita já criada.</p>
             </div>
 
-            <div class="w-1/4 bg-white rounded-lg p-20 shadow-lg flex flex-col items-start gap-3">
+            <div class="w-1/4 bg-white rounded-3xl p-20 shadow-2xl flex flex-col items-start gap-3">
                 <img src="{{asset('images/config-image.png')}}" alt="" class="h-15 w-14">
                 <p class="font-bold text-2xl">Configurações</p>
                 <p class="text-lg text-gray-600">Organize acessos, privilégios e ajustes do sistema</p>
-            </div>
-            
+            </div>            
          </div>            
     </div>
+    <div class="items-center text-center">
+                <p class="text-gray-700 mt-20">© Todos os Direitos Reservados E-Supply 2025.</p>
+            </div>
 </x-app-layout>
